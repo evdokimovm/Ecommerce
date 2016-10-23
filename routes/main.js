7ee5eb3b43d1b245dbced6304d7e77fe073965d2
evdokimovm <tel89147925532@gmail.com>
2016-10-22 22:23:29 -0700

    Products Route and Products Page

--- routes/main.js
+++ routes/main.js
@@ -1,4 +1,5 @@
 var router = require('express').Router()
+var Product = require('../models/product')
 
 router.get('/', function(req, res, next) {
 	res.render('main/home')
@@ -6,6 +7,17 @@ router.get('/', function(req, res, next) {
 
 router.get('/about', function(req, res, next) {
 	res.render('main/about')
+})
+
+router.get('/products/:id', function(req, res, next) {
+	Product.find({
+		category: req.params.id
+	}).populate('category').exec(function(err, products) {
+		if (err) return next(err)
+		res.render('main/category', {
+			products: products
+		})
+	})
 })
 
 module.exports = router
