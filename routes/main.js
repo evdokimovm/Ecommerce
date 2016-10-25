063fe62e23b40e8d80e8cc88c4cd6cb8634fc332
evdokimovm <tel89147925532@gmail.com>
2016-10-24 22:51:53 -0700

    Pagination

--- routes/main.js
+++ routes/main.js
@@ -1,6 +1,26 @@
 var router = require('express').Router()
 var Product = require('../models/product')
 
+function paginate(req, res, next) {
+	var perPage = 9
+	var page = req.params.page
+
+	Product
+		.find({})
+		.skip(perPage * page)
+		.limit(perPage)
+		.populate('category')
+		.exec(function(err, products) {
+			Product.count().exec(function(err, count) {
+				if (err) return next(err)
+				res.render('main/product-main', {
+					products: products,
+					pages: count / perPage
+				})
+			})
+		})
+}
+
 Product.createMapping(function(err, mapping) {
 	if (err) {
 		console.log('Error Creating Mapping')
@@ -50,7 +70,15 @@ router.get('/search', function(req, res, next) {
 })
 
 router.get('/', function(req, res, next) {
-	res.render('main/home')
+	if (req.user) {
+		paginate(req, res, next)
+	} else {
+		res.render('main/home')
+	}
+})
+
+router.get('/page/:page', function(req, res, next) {
+	paginate(req, res, next)
 })
 
 router.get('/about', function(req, res, next) {
