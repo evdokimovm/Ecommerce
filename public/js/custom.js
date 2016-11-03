2a9e44463363f0653d1bb66feb8ef79d6db61327
evdokimovm <tel89147925532@gmail.com>
2016-11-03 01:32:55 -0700

    Adding Product to Cart

--- public/js/custom.js
+++ public/js/custom.js
@@ -39,4 +39,35 @@ $(function() {
 			}
 		})
 	})
+
+	$('#plus').on('click', function(event) {
+		event.preventDefault()
+		var priceValue = parseFloat($('#priceValue').val())
+		var quantity = parseInt($('#quantity').val())
+
+		priceValue += parseFloat($('#priceHidden').val())
+		quantity += 1
+
+		$('#quantity').val(quantity)
+		$('#priceValue').val(priceValue.toFixed(2))
+		$('#total').html(quantity)
+	})
+
+	$('#minus').on('click', function(event) {
+		event.preventDefault()
+		var priceValue = parseFloat($('#priceValue').val())
+		var quantity = parseInt($('#quantity').val())
+
+		if(quantity > 1) {
+			quantity -= 1
+			priceValue -= parseFloat($('#priceHidden').val())
+		} else if(quantity == 1) {
+			quantity = 1
+			priceValue = parseFloat($('#priceHidden').val())
+		}
+
+		$('#quantity').val(quantity)
+		$('#priceValue').val(priceValue.toFixed(2))
+		$('#total').html(quantity)
+	})
 })
--- routes/main.js
+++ routes/main.js
@@ -1,5 +1,6 @@
 var router = require('express').Router()
 var Product = require('../models/product')
+var Cart = require('../models/cart')
 
 function paginate(req, res, next) {
 	var perPage = 9
@@ -44,6 +45,39 @@ stream.on('close', function() {
 
 stream.on('error', function(err) {
 	console.log(err)
+})
+
+router.get('/cart', function(req, res, next) {
+	Cart
+		.findOne({
+			owner: req.user._id
+		})
+		.populate('items.item')
+		.exec(function(err, foundCart) {
+			if(err) return next(err)
+			res.render('main/cart', {
+				foundCart: foundCart
+			})
+		})
+})
+
+router.post('/product/:product_id', function(req, res, next) {
+	Cart.findOne({
+		owner: req.user._id
+	}, function(err, cart) {
+		cart.items.push({
+			item: req.body.product_id,
+			price: parseFloat(req.body.priceValue),
+			quantity: parseInt(req.body.quantity)
+		})
+
+		cart.total = (cart.total + parseFloat(req.body.priceValue)).toFixed(2)
+
+		cart.save(function(err) {
+			if (err) return next(err)
+			return res.redirect('/cart')
+		})
+	})
 })
 
 router.post('/search', function(req, res, next) {
