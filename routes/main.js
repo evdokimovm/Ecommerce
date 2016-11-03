a4b4666c547bb377aa94cc1f7b8c1e9aafe084d2
evdokimovm <tel89147925532@gmail.com>
2016-11-03 04:20:52 -0700

    Remove Product from Cart

--- routes/main.js
+++ routes/main.js
@@ -56,7 +56,8 @@ router.get('/cart', function(req, res, next) {
 		.exec(function(err, foundCart) {
 			if(err) return next(err)
 			res.render('main/cart', {
-				foundCart: foundCart
+				foundCart: foundCart,
+				message: req.flash('remove')
 			})
 		})
 })
@@ -76,6 +77,22 @@ router.post('/product/:product_id', function(req, res, next) {
 		cart.save(function(err) {
 			if (err) return next(err)
 			return res.redirect('/cart')
+		})
+	})
+})
+
+router.post('/remove', function(req, res, next) {
+	Cart.findOne({
+		owner: req.user._id
+	}, function(err, foundCart) {
+		foundCart.items.pull(String(req.body.item))
+
+		foundCart.total = (foundCart.total - parseFloat(req.body.price)).toFixed(2)
+
+		foundCart.save(function(err, found) {
+			if (err) return next(err)
+			req.flash('remove', 'Successfully removed')
+			res.redirect('/cart')
 		})
 	})
 })
