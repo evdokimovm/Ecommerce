e708ab7b527dc79aa9546ba46a11b62dc5261c0b
evdokimovm <tel89147925532@gmail.com>
2016-11-24 07:14:10 -0800

    Add New Route for Add Product to Category. Now Add Products Manually Without Using Faker

--- api/api.js
+++ api/api.js
@@ -1,8 +1,5 @@
 var router = require('express').Router()
-var async = require('async')
-var faker = require('faker')
 var secret = require('../config/secret')
-var Category = require('../models/category')
 var Product = require('../models/product')
 var User = require('../models/user')
 
@@ -63,34 +60,6 @@ router.post('/change-password', function(req, res, next) {
 			req.flash('error', 'You Need to Fill Old Password and New Password Fields to Change Password')
 			return res.redirect('/edit-profile')
 		}
-	})
-})
-
-router.get('/:name', function(req, res, next) {
-	async.waterfall([
-		function(callback) {
-			Category.findOne({
-				name: req.params.name
-			}, function(err, category) {
-				if (err) return next(err)
-				callback(null, category)
-			})
-		},
-		function(category, callback) {
-			for (var i = 0; i < 30; i++) {
-				var product = new Product()
-
-				product.category = category._id
-				product.name = faker.commerce.productName()
-				product.price = faker.commerce.price()
-				product.image = faker.image.image()
-
-				product.save()
-			}
-		}
-	])
-	res.json({
-		message: 'Success'
 	})
 })
 
--- routes/admin.js
+++ routes/admin.js
@@ -1,9 +1,13 @@
 var router = require('express').Router()
+var async = require('async')
+var faker = require('faker')
 var Category = require('../models/category')
+var Product = require('../models/product')
 
 router.get('/add-category', function(req, res, next) {
 	res.render('admin/add-category', {
-		message: req.flash('success')
+		message: req.flash('success'),
+		error: req.flash('error')
 	})
 })
 
@@ -17,6 +21,44 @@ router.post('/add-category', function(req, res, next) {
 		req.flash('success', 'Successfully Added Category')
 		return res.redirect('/add-category')
 	})
+})
+
+router.get('/add-product', function(req, res, next) {
+	res.render('admin/add-product', {
+		message: req.flash('success')
+	})
+})
+
+router.post('/add-product', function(req, res, next) {
+	async.waterfall([
+		function(callback) {
+			Category.findOne({
+				name: req.body.category_name
+			}, function(err, category) {
+				if (err) return next(err)
+
+				if (!category || category === 'null' || category === 'undefined') {
+					req.flash('error', 'First, You Need to Create this Category Here')
+					res.redirect('/add-category')
+				} else {
+					callback(null, category)
+				}
+			})
+		},
+		function(category) {
+			var product = new Product()
+
+			product.category = category._id
+			product.name = req.body.product_name
+			product.price = req.body.product_price
+			product.image = faker.image.image()
+
+			product.save()
+
+			req.flash('success', 'Success Adding Product')
+			res.redirect('/add-product')
+		}
+	])
 })
 
 module.exports = router
